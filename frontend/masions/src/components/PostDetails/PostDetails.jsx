4aaec9d8047f3d7c65c6ca2cb4d31586677f76e1
AhmedinhoY <ahmedinho.y@gmail.com>
2024-11-06 18:39:26 +0300

    some modifications

--- frontend/masions/src/components/PostDetails/PostDetails.jsx
+++ frontend/masions/src/components/PostDetails/PostDetails.jsx
@@ -176,31 +176,27 @@ export default function Example() {
         <div className="mx-auto mt-6 max-w-2xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-3 lg:gap-x-8 lg:px-8">
           <div className="aspect-h-4 aspect-w-3 hidden overflow-hidden rounded-lg lg:block">
             <img
-              alt={product.images[0].alt}
-              src={product.images[0].src}
+              src={houses[0].img[0].imgSrc}
               className="h-full w-full object-cover object-center"
             />
           </div>
           <div className="hidden lg:grid lg:grid-cols-1 lg:gap-y-8">
             <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
               <img
-                alt={product.images[1].alt}
-                src={product.images[1].src}
+                src={houses[0].img[1].imgSrc}
                 className="h-full w-full object-cover object-center"
               />
             </div>
             <div className="aspect-h-2 aspect-w-3 overflow-hidden rounded-lg">
               <img
-                alt={product.images[2].alt}
-                src={product.images[2].src}
+                src={houses[0].img[2].imgSrc}
                 className="h-full w-full object-cover object-center"
               />
             </div>
           </div>
           <div className="aspect-h-5 aspect-w-4 lg:aspect-h-4 lg:aspect-w-3 sm:overflow-hidden sm:rounded-lg">
             <img
-              alt={product.images[3].alt}
-              src={product.images[3].src}
+              src={houses[0].img[3].imgSrc}
               className="h-full w-full object-cover object-center"
             />
           </div>
