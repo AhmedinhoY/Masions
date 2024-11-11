c89f8526764d3da18100ac563d8277c6fc8ff15d
AhmedinhoY <ahmedinho.y@gmail.com>
2024-11-11 18:19:52 +0300

    added Similar Properties Section to the Details Page

--- frontend/masions/src/routes/PropertiesList/PropertiesList.jsx
+++ frontend/masions/src/routes/PropertiesList/PropertiesList.jsx
@@ -1,25 +1,42 @@
 import { ImageSlider } from "../../components/ImageSlider/ImageSlider";
 import "./PropertiesList.css";
 
-const PropertiesList = ({ propertyType } = props) => {
+const PropertiesList = ({ propertyType, limit }) => {
   return (
     <div className="products-grid">
-      {propertyType.map((property) => (
-        <div key={property.id} className="group">
-          <div className="product-img-container">
-            <ImageSlider
-              imgCollection={property.img}
-              propertyID={property.id}
-            />
-          </div>
-          <h2>{property.city}</h2>
-          <h3>{property.type} for Sale</h3>
-          <h3>
-            {property.area} m<sup>2</sup> - {property.bedrooms} bedrooms
-          </h3>
-          <p className="mt-1">{property.price.toLocaleString()} BD</p>
-        </div>
-      ))}
+      {limit
+        ? propertyType.slice(0, limit).map((property) => (
+            <div key={property.id} className="group">
+              <div className="product-img-container">
+                <ImageSlider
+                  imgCollection={property.img}
+                  propertyID={property.id}
+                />
+              </div>
+              <h2>{property.city}</h2>
+              <h3>{property.type} for Sale</h3>
+              <h3>
+                {property.area} m<sup>2</sup> - {property.bedrooms} bedrooms
+              </h3>
+              <p className="mt-1">{property.price.toLocaleString()} BD</p>
+            </div>
+          ))
+        : propertyType.map((property) => (
+            <div key={property.id} className="group">
+              <div className="product-img-container">
+                <ImageSlider
+                  imgCollection={property.img}
+                  propertyID={property.id}
+                />
+              </div>
+              <h2>{property.city}</h2>
+              <h3>{property.type} for Sale</h3>
+              <h3>
+                {property.area} m<sup>2</sup> - {property.bedrooms} bedrooms
+              </h3>
+              <p className="mt-1">{property.price.toLocaleString()} BD</p>
+            </div>
+          ))}
     </div>
   );
 };
